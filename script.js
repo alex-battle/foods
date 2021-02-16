69d8154b36ee19ef9ffd61d8925c89c5a6e988ed
Alex Battle <58659536+alex-battle@users.noreply.github.com>
2021-02-16 17:16:54 -0500

    style landing page and create slideshow for the first section

---
+++ script.js
@@ -0,0 +1,26 @@
+const icons = document.querySelectorAll('.section-1-icons i')
+
+let i = 1
+// counter variable
+// on each change this counter should increase by one
+
+
+setInterval(() => {
+    // set interval for icons to do a slideshow of all images every second 
+    // 2 arguments (function, time in milliseconds)
+
+    i++
+
+    const icon = document.querySelector('.section-1-icons .change')
+    // select the element
+    icon.classList.remove('change')
+    // takes the class change off of the egg class that has a class of change
+    if (i > icons.length){
+        icons[0].classList.add('change')
+        i = 1
+    } else{
+    icon.nextElementSibling.classList.add('change')
+    }
+}, 2000)
+// 1000 is 1 second
+
